a0f484cbc9b70a6f9f78fe2cac73b8a5e1b153a4
Bang <hbtruong.2017@sis.smu.edu.sg>
2019-10-30 02:51:53 +0800

    Update routing for API endpoints

--- central-server/api-routes.js
+++ central-server/api-routes.js
@@ -1,29 +1,32 @@
 // Initialize express router
-let router = require('express').Router();
+let router = require("express").Router();
 // Set default API response
-router.get('/', function (req, res) {
-    res.json({
-        status: 'API Its Working',
-        message: 'Welcome to RESTHub crafted with love!'
-    });
+router.get("/", function(req, res) {
+  res.json({
+    status: "API Its Working",
+    message: "Welcome to RESTHub crafted with love!"
+  });
 });
 
 // Import employee controller
-var employeeController = require('./controller/employeeController');
+var employeeController = require("./controller/employeeController");
 // Contact routes
-router.route('/employees')
-    .get(employeeController.index)
-    .post(employeeController.new);
-router.route('/employees/:id')
-    .get(employeeController.view)
-    .patch(employeeController.update)
-    .put(employeeController.update)
-    .delete(employeeController.delete);
+router
+  .route("/employees")
+  .get(employeeController.index)
+  .post(employeeController.new);
+router
+  .route("/employees/:id")
+  .get(employeeController.view)
+  .patch(employeeController.update)
+  .put(employeeController.update)
+  .delete(employeeController.delete);
 
-var timesheetController = require('./controller/timesheetController')
-router.route('/timesheet')
-    .post(timesheetController.clockIn);
-router.route('/timesheet/:id')
-    .put(timesheetController.clockOut);
+var timesheetController = require("./controller/timesheetController");
+router.route("/timesheet").post(timesheetController.clockIn);
+router
+  .route("/timesheet/:id")
+  .get(timesheetController.getTimesheet)
+  .put(timesheetController.clockOut);
 // Export API routes
 module.exports = router;
