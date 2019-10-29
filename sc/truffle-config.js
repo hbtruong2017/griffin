27e2af58421c721b4c79f97e2a3336935be745cc
Bang <hbtruong.2017@sis.smu.edu.sg>
2019-10-29 23:57:01 +0800

    Update configuration file for truffle

--- sc/truffle-config.js
+++ sc/truffle-config.js
@@ -2,13 +2,18 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    },
-    develop: {
-      port: 8545
+    // development: {
+    //   host: "127.0.0.1",
+    //   port: 8545,
+    //   network_id: "*" // Match any network id
+    // },
+    // develop: {
+    //   port: 8545
+    // },
+    ganache: {
+      host: "localhost",
+      port: 7545, // By default Ganache runs on this port.
+      network_id: "*" // network_id for ganache is 5777. However, by keeping * as value you can run this node on any network
     }
   }
 };
