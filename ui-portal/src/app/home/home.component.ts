74a33f9c10db1a975248a5ec20d6e5522694050c
Bang <hbtruong.2017@sis.smu.edu.sg>
2019-10-30 02:53:21 +0800

    Insert form for description

--- ui-portal/src/app/home/home.component.ts
+++ ui-portal/src/app/home/home.component.ts
@@ -10,18 +10,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
   employeeId: any;
+  timesheetId: any;
   employeeDetail: any;
   clockOutForm: FormGroup;
+  timesheet: any;
+  duration: any;
+  salaryRate: any;
 
   constructor(private dataService: DataService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     // this.employeeId = "5daee7026fb70b0f746dd37b";
-    this.employeeId = window.sessionStorage.getItem("timesheetId");
-    this.dataService.getEmployeeDetails(this.employeeId).subscribe((data: any) => {
-      this.employeeDetail = data.data;
-      console.log(this.employeeDetail)
-      window.sessionStorage.setItem("employeeDetail", JSON.stringify(data.data))
+    this.employeeId = window.sessionStorage.getItem("employeeId");
+    this.timesheetId = window.sessionStorage.getItem("timesheetId");
+    this.salaryRate = window.sessionStorage.getItem("salaryRate");
+
+    this.dataService.getTimesheet(this.timesheetId).subscribe((data: any) => {
+      this.timesheet = data.data;
+      this.duration = data.duration;
+      console.log(this.timesheet)
     })
 
     this.clockOutForm = this.formBuilder.group({
