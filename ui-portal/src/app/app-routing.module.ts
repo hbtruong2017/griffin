775e2dd18cf7c018ac59474941d9cb91003c0106
Bang <hbtruong.2017@sis.smu.edu.sg>
2019-10-30 11:59:47 +0800

    Update routing for login

--- ui-portal/src/app/app-routing.module.ts
+++ ui-portal/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { PreHomeComponent } from './pre-home/pre-home.component';
 import { PaymentSuccessComponent } from './payment-success/payment-success.component';
 import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
-
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'profile' },  
@@ -13,8 +13,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'paymentsuccess', component: PaymentSuccessComponent },
-  { path: 'history', component: TransactionHistoryComponent }
-
+  { path: 'history', component: TransactionHistoryComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- ui-portal/src/app/app.module.ts
+++ ui-portal/src/app/app.module.ts
@@ -5,10 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './home/home.component';
-import { SplashComponent } from './splash/splash.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { SubmitLoanComponent } from './submit-loan/submit-loan.component';
 import { ProfileComponent } from './profile/profile.component';
 import { PreHomeComponent } from './pre-home/pre-home.component';
 import { PaymentSuccessComponent } from './payment-success/payment-success.component';
@@ -20,8 +18,6 @@ import { LoginComponent } from './login/login.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SplashComponent,
-    SubmitLoanComponent,
     ProfileComponent,
     PreHomeComponent,
     PaymentSuccessComponent,
